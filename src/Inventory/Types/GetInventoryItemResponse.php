2b2ecd0d82af09f1cda041b55606100c779f59bf
zheng wang <z.wang@nri-isd.com>
2022-03-25 15:20:09 -0400

    fix statusCode in GetInventoryItemResponse

--- src/Inventory/Types/GetInventoryItemResponse.php
+++ src/Inventory/Types/GetInventoryItemResponse.php
@@ -2,9 +2,6 @@
 
 namespace DTS\eBaySDK\Inventory\Types;
 
-use DTS\eBaySDK\HttpHeadersTrait;
-use DTS\eBaySDK\StatusCodeTrait;
-
 /**
  * @property \DTS\eBaySDK\Inventory\Types\ErrorDetailV3 $errors
  * @property \DTS\eBaySDK\Inventory\Types\InventoryItemWithSkuLocaleGroupKeys $inventoryItem
@@ -15,49 +12,49 @@ use DTS\eBaySDK\StatusCodeTrait;
 
 class GetInventoryItemResponse extends \DTS\eBaySDK\Types\BaseType
 {
-	use StatusCodeTrait;
-	use HttpHeadersTrait;
+    /**
+     * @var array Properties belonging to objects of this class.
+     */
+    private static $propertyTypes = [
+        'errors' => [
+            'type' => 'DTS\eBaySDK\Inventory\Types\ErrorDetailV3',
+            'repeatable' => true,
+            'attribute' => false,
+            'elementName' => 'errors',
+        ],
+        'inventoryItem' => [
+            'type' => 'DTS\eBaySDK\Inventory\Types\InventoryItemWithSkuLocaleGroupKeys',
+            'repeatable' => false,
+            'attribute' => false,
+            'elementName' => 'inventoryItem',
+        ],
+        'sku' => [
+            'type' => 'string',
+            'repeatable' => false,
+            'attribute' => false,
+            'elementName' => 'sku',
+        ],
+        'statusCode' => [
+            'type' => 'integer',
+            'repeatable' => false,
+            'attribute' => false,
+            'elementName' => 'statusCode',
+        ],
+    ];
 
-	private static $propertyTypes = [
-		'errors' => [
-			'type' => 'DTS\eBaySDK\Inventory\Types\ErrorDetailV3',
-			'repeatable' => true,
-			'attribute' => false,
-			'elementName' => 'errors'
-		],
-		'inventoryItem' => [
-			'type' => 'DTS\eBaySDK\Inventory\Types\InventoryItemWithSkuLocaleGroupKeys',
-			'repeatable' => false,
-			'attribute' => false,
-			'elementName' => 'inventoryItem'
-		],
-		'sku' => [
-			'type' => 'string',
-			'repeatable' => false,
-			'attribute' => false,
-			'elementName' => 'sku'
-		]
-	];
+    /**
+     * @param array $values Optional properties and values to assign to the object.
+     */
+    public function __construct(array $values = [])
+    {
+        list($parentValues, $childValues) = self::getParentValues(self::$propertyTypes, $values);
 
-	/**
-	 * @param array $values Optional properties and values to assign to the object.
-	 * @param int $statusCode Status code
-	 * @param array $headers HTTP Response headers.
-	 */
-	public function __construct(array $values = [], $statusCode = 200, array $headers = [])
-	{
-		list($parentValues, $childValues) = self::getParentValues(self::$propertyTypes, $values);
+        parent::__construct($parentValues);
 
-		parent::__construct($parentValues);
+        if (!array_key_exists(__CLASS__, self::$properties)) {
+            self::$properties[__CLASS__] = array_merge(self::$properties[get_parent_class()], self::$propertyTypes);
+        }
 
-		if (!array_key_exists(__CLASS__, self::$properties)) {
-			self::$properties[__CLASS__] = array_merge(self::$properties[get_parent_class()], self::$propertyTypes);
-		}
-
-		$this->setValues(__CLASS__, $childValues);
-
-		$this->statusCode = (int)$statusCode;
-
-		$this->setHeaders($headers);
-	}
+        $this->setValues(__CLASS__, $childValues);
+    }
 }
